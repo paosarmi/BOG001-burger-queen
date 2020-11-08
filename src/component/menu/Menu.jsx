4e707cebb3e67f324cca2b08ee34c5296f8d87b9
paosarmi <paosarmi270218@hotmail.com>
2020-11-07 19:29:56 -0500

    show json data in order view

--- src/component/menu/Menu.jsx
+++ src/component/menu/Menu.jsx
@@ -1,32 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Menu from "../../menu.json";
 import "./Menu.scss";
 
 const MenuOrder = () => {
-
-
-    const [breakfast, setBreakfast] = React.useState([])
-
-    React.useEffect(() => {
-        /* console.log("useEffect") */
-        getMenu()
-    }, [])
-
-
-    const getMenu = async () => {
-        const productsMenu = await { Menu }
-
-        /* console.log(productsMenu); */
-        setBreakfast(productsMenu)
-    }
 
     return (
         <div>
             <h1>Pedidos</h1>
             <ul>
                 {
-                    breakfast.map(item => (
-                        <li key={item.queenBreakfast}>{item.bebidas} - {item.sandwich}</li>
+                    Menu.queenBreakfast.bebidas.map(item => (
+                        <li >{item.product}-{item.price} </li>
                     ))
                 }
             </ul>

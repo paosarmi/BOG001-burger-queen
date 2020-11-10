341f2a0eab7ff966119c464c8f3b08c39fb37a4a
DayanaM <dmmartinez269@misena.edu.co>
2020-11-10 13:12:41 -0500

    Firebase initialize App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import App from "./App";
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDA4YS4_whEO-CujoeNR9G7gfrAw2BE3AQ",
+  authDomain: "burguerqueen-6dc2c.firebaseapp.com",
+  databaseURL: "https://burguerqueen-6dc2c.firebaseio.com",
+  projectId: "burguerqueen-6dc2c",
+  storageBucket: "burguerqueen-6dc2c.appspot.com",
+  messagingSenderId: "41311849012",
+  appId: "1:41311849012:web:d6d1458957c9eeb3709861",
+  measurementId: "G-EG149785SG",
+};
+
+firebase.initializeApp(firebaseConfig);
+//firebase deploy --only hosting:burguer-queen-wood
 
 ReactDOM.render(
   <React.StrictMode>

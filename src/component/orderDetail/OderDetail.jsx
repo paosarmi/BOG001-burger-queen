cb9fa4556d72596f6dfd574a399610ff194f57eb
DayanaM <dmmartinez269@misena.edu.co>
2020-11-11 20:34:51 -0500

    State order layout

--- src/component/orderDetail/OderDetail.jsx
+++ src/component/orderDetail/OderDetail.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
+import React from 'react';
 import "./OrderDetail.scss";
+import { Redirect } from 'react-router-dom'
 
-function OrderDetail() {
-    return (
-        <div className="Order-detail-container">
-            <div className="Order-products-container">
-                <p>Cafe américano    1    $5</p>
-            </div>
-            <div className="Order-total-container">
-                <p>Total    5</p>
-            </div>
-            <div className="Submit-container">
-                <button className="Submit-order">Enviar a cocina</button>
+class OrderDetail extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            redirect: false,
+        };
+    }
+    render() {
+        if (this.state.redirect) {
+            return <Redirect to={this.state.redirect} />
+        }
+        return (
+            <div className="Order-detail-container">
+                <div className="Order-products-container">
+                    <p>Cafe américano    1    $5</p>
+                </div>
+                <div className="Order-total-container">
+                    <p>Total    5</p>
+                </div>
+                <div className="Submit-container">
+                    <button className="Submit-order" onClick={() => this.OrderState()}>Enviar a cocina</button>
+                </div >
             </div >
-        </div >
-    )
+        )
+    }
+    OrderState() {
+        this.setState({ redirect: "/order-state" })
+    }
 }
 export default OrderDetail;

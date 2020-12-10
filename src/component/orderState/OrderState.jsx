01533da0cd78b996300bee57b836b7062da57ade
DayanaM <dmmartinez269@misena.edu.co>
2020-12-10 10:00:19 -0500

    Path state order

--- src/component/orderState/OrderState.jsx
+++ src/component/orderState/OrderState.jsx
@@ -4,23 +4,38 @@ import BackButton from "../backButton/BackButton";
 import TableState from "./state.png"
 import "./OrderState.scss";
 
-function OrderState() {
-    return (
-        <div className="Waiters">
-            <div className="Intro-body">
-                <div className="Order-state">
-                    <WoodLogo />
-                    <div>
-                        <BackButton path="/order" />
-                    </div>
-                    <div className="State-container">
-                        <div className="Table-container">
-                            <img src={TableState} alt="Tabla de pedidos" className="Table-order" />
+class OrderState extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            redirect: false
+        };
+    }
+    render() {
+        if (this.state.redirect) {
+            return <Redirect to={this.state.redirect} />
+        }
+        return (
+            <div className="Waiters" >
+                <div className="Intro-body">
+                    <div className="Order-state">
+                        <WoodLogo />
+                        <div>
+                            <BackButton path="/order" />
+                        </div>
+                        <div className="State-container">
+                            <div className="Table-container">
+                                <img src={TableState} alt="Tabla de pedidos" className="Table-order" />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+        home() {
+            this.setState({ redirect: "/" })
+        }
+    }
 }
 export default OrderState;
